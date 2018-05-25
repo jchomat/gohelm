2479be0613e0d3afba9f9339789c191659b699af
Pablo RUTH <contact@pablo-ruth.fr>
2018-05-25 15:33:23 +0200

    add list release by status and delete release by name

--- delete.go
+++ delete.go
@@ -3,17 +3,30 @@ package gohelm
 import (
 	"fmt"
 
-	tiller "k8s.io/helm/pkg/proto/hapi/services"
+	"k8s.io/helm/pkg/proto/hapi/release"
+	"k8s.io/helm/pkg/proto/hapi/services"
 )
+
+func (c *Client) DeleteRelease(name string) error {
+	sv := services.NewReleaseServiceClient(c.Conn)
+
+	uniReq := &services.UninstallReleaseRequest{
+		Name:  release.GetName(),
+		Purge: true,
+	}
+	_, err := sv.UninstallRelease(c.Context, uniReq)
+
+	return err
+}
 
 func (c *Client) DeleteReleasesInNamespace(namespace string) error {
 	// Get Helm release in current namespace
-	realReq := &tiller.ListReleasesRequest{
-		SortBy:    tiller.ListSort_NAME,
-		SortOrder: tiller.ListSort_ASC,
+	realReq := &services.ListReleasesRequest{
+		SortBy:    services.ListSort_NAME,
+		SortOrder: services.ListSort_ASC,
 		Namespace: namespace,
 	}
-	sv := tiller.NewReleaseServiceClient(c.Conn)
+	sv := services.NewReleaseServiceClient(c.Conn)
 	res, err := sv.ListReleases(c.Context, realReq)
 	if err != nil {
 		return err
@@ -29,7 +42,7 @@ func (c *Client) DeleteReleasesInNamespace(namespace string) error {
 
 	// Delete releases in current namespace
 	for _, release := range rec.Releases {
-		uniReq := &tiller.UninstallReleaseRequest{
+		uniReq := &services.UninstallReleaseRequest{
 			Name:  release.GetName(),
 			Purge: true,
 		}
---
+++ list.go
@@ -0,0 +1,39 @@
+package gohelm
+
+import (
+	"k8s.io/helm/pkg/proto/hapi/release"
+	"k8s.io/helm/pkg/proto/hapi/services"
+)
+
+func (c *Client) ListReleasesByStatus(status int) ([]*release.Release, error) {
+
+	var allReleases []*release.Release
+	var offset string
+
+	sv := services.NewReleaseServiceClient(c.Conn)
+
+	for {
+		// Get Helm releases with specific status
+		realReq := &services.ListReleasesRequest{
+			StatusCodes: status,
+			Limit:       10,
+			Offset:      offset,
+		}
+		res, err := sv.ListReleases(c.Context, realReq)
+		if err != nil {
+			return allReleases, err
+		}
+		rec, err := res.Recv()
+		if err != nil {
+			return allReleases, err
+		}
+
+		allReleases = append(allReleases, rec.Releases...)
+
+		if rec.Count < 10 {
+			break
+		}
+	}
+
+	return allReleases, nil
+}
