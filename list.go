6bcdd712cbcc0daec37c983209794662773efe75
jchomat <jeremy.chomat@ouest-france.fr>
2021-04-22 07:31:57 +0200

    Add ListReleasesByStatusAndNamespace Function
    Only ListReleasesRequest has changed

--- list.go
+++ list.go
@@ -50,3 +50,45 @@ func (c *Client) ListReleasesByStatus(ctx context.Context, status []release.Stat
 
 	return releases, nil
 }
+
+func (c *Client) ListReleasesByStatusAndNamespace(ctx context.Context, status []release.Status_Code, namespace string) ([]*release.Release, error) {
+
+	// Get Helm releases with specific status
+	realReq := &services.ListReleasesRequest{
+		StatusCodes: status,
+		Namespace:   namespace,
+	}
+	sv := services.NewReleaseServiceClient(c.Conn)
+
+	// Config helm version header
+	md := metadata.Pairs("x-helm-api-client", c.Version)
+	helmCtx := metadata.NewOutgoingContext(ctx, md)
+
+	res, err := sv.ListReleases(helmCtx, realReq)
+	if err != nil {
+		return nil, fmt.Errorf("failed to list release: %s", err)
+	}
+
+	var releases []*release.Release
+	for {
+		rec, err := res.Recv()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return nil, fmt.Errorf("failed to read release list grpc response: %s", err)
+		}
+
+		releases = append(releases, rec.Releases...)
+
+		select {
+		case <-helmCtx.Done():
+			break
+		default:
+			continue
+		}
+	}
+
+	return releases, nil
+}
